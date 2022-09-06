1ab1202c54c211f33b96cbb95ff4c1a0707d1160
pattaraphon <pattarapon9741@gmail.com>
2022-09-07 02:43:55 +0700

    first commit

---
+++ posts.js
@@ -0,0 +1,162 @@
+module.exports = [
+  {
+    "id": "63179f82d04f7c629f9790f1",
+    "email": "margaret_jarvis@duoflex.sucks",
+    "roles": [
+      "guest",
+      "owner"
+    ],
+    "apiKey": "c4eb6ea8-51c8-40dd-8b25-b231b588a93b",
+    "profile": {
+      "dob": "1994-05-23",
+      "name": "Margaret Jarvis",
+      "about": "Elit esse officia veniam elit elit aute eiusmod aute mollit qui commodo eu incididunt fugiat. Aliqua nulla occaecat fugiat Lorem officia in nostrud ipsum non excepteur mollit.",
+      "address": "69 Sutter Avenue, Joes, Maine",
+      "company": "Duoflex",
+      "location": {
+        "lat": 77.296247,
+        "long": 63.922038
+      }
+    },
+    "username": "margaret94",
+    "createdAt": "2011-10-26T18:37:28.944Z",
+    "updatedAt": "2011-10-27T18:37:28.944Z"
+  },
+  {
+    "id": "63179f82b3c5c7d53f15b393",
+    "email": "robin_gaines@fibrodyne.kddi",
+    "roles": [
+      "admin",
+      "member"
+    ],
+    "apiKey": "31ea9cb4-3a73-4081-bdb4-6880680fffc8",
+    "profile": {
+      "dob": "1992-07-25",
+      "name": "Robin Gaines",
+      "about": "Ex pariatur et est ut quis amet. Cillum qui nostrud aliquip nisi non fugiat adipisicing sint Lorem consectetur.",
+      "address": "51 Sunnyside Court, Marysville, Georgia",
+      "company": "Fibrodyne",
+      "location": {
+        "lat": -71.832405,
+        "long": 103.335208
+      }
+    },
+    "username": "robin92",
+    "createdAt": "2011-04-20T08:09:58.693Z",
+    "updatedAt": "2011-04-21T08:09:58.693Z"
+  },
+  {
+    "id": "63179f82a51d114194a2fd15",
+    "email": "daugherty_jensen@eweville.network",
+    "roles": [
+      "admin",
+      "guest"
+    ],
+    "apiKey": "dc5d4604-0f50-4fde-9ae3-bc5da7328315",
+    "profile": {
+      "dob": "1994-02-25",
+      "name": "Daugherty Jensen",
+      "about": "Aliquip enim laborum anim id excepteur et dolore ullamco. Enim qui cupidatat excepteur in incididunt est commodo laborum est ut.",
+      "address": "75 Lott Street, Gouglersville, Kansas",
+      "company": "Eweville",
+      "location": {
+        "lat": -48.934702,
+        "long": -40.239405
+      }
+    },
+    "username": "daugherty94",
+    "createdAt": "2011-06-17T01:48:03.165Z",
+    "updatedAt": "2011-06-18T01:48:03.165Z"
+  },
+  {
+    "id": "63179f8257f45744ff29ef7b",
+    "email": "kerri_hale@eventage.industries",
+    "roles": [
+      "guest"
+    ],
+    "apiKey": "f9439b12-9046-4f9a-a27d-237d9f749564",
+    "profile": {
+      "dob": "1994-07-02",
+      "name": "Kerri Hale",
+      "about": "Exercitation minim nulla commodo qui dolore quis. Proident anim quis minim amet occaecat officia nostrud.",
+      "address": "93 Cropsey Avenue, Ezel, Indiana",
+      "company": "Eventage",
+      "location": {
+        "lat": -63.588584,
+        "long": 97.126877
+      }
+    },
+    "username": "kerri94",
+    "createdAt": "2013-06-11T11:21:10.650Z",
+    "updatedAt": "2013-06-12T11:21:10.650Z"
+  },
+  {
+    "id": "63179f822f92ddd35fff3009",
+    "email": "sara_mccullough@uni.nec",
+    "roles": [
+      "guest",
+      "member"
+    ],
+    "apiKey": "67a38226-fbf8-4dfb-8140-6e624ce859e7",
+    "profile": {
+      "dob": "1993-03-29",
+      "name": "Sara Mccullough",
+      "about": "Nostrud ea minim nulla occaecat consectetur aute non. Occaecat amet reprehenderit elit aliquip deserunt eu nulla commodo ea ipsum sint laborum.",
+      "address": "85 Frost Street, Sunwest, Arizona",
+      "company": "Uni",
+      "location": {
+        "lat": 6.507716,
+        "long": 172.749464
+      }
+    },
+    "username": "sara93",
+    "createdAt": "2012-07-22T00:27:00.768Z",
+    "updatedAt": "2012-07-23T00:27:00.768Z"
+  },
+  {
+    "id": "63179f825d6fe98f7e5bfbff",
+    "email": "webb_patel@voratak.catering",
+    "roles": [
+      "guest",
+      "member"
+    ],
+    "apiKey": "e8171e16-e692-4f14-8531-ab5eeab484dd",
+    "profile": {
+      "dob": "1990-10-21",
+      "name": "Webb Patel",
+      "about": "Labore dolore mollit voluptate et et incididunt pariatur amet nulla nostrud exercitation ipsum. Cillum est dolor ut ut occaecat consequat consequat id amet proident.",
+      "address": "12 Myrtle Avenue, Darbydale, Nebraska",
+      "company": "Voratak",
+      "location": {
+        "lat": 29.269682,
+        "long": -49.804553
+      }
+    },
+    "username": "webb90",
+    "createdAt": "2011-04-30T05:17:55.677Z",
+    "updatedAt": "2011-05-01T05:17:55.677Z"
+  },
+  {
+    "id": "63179f82ebd8119fae5423ea",
+    "email": "cindy_woodward@fitcore.dm",
+    "roles": [
+      "owner",
+      "guest"
+    ],
+    "apiKey": "01f452d2-62fe-4e3e-8278-4d69ea1e195d",
+    "profile": {
+      "dob": "1992-05-08",
+      "name": "Cindy Woodward",
+      "about": "Tempor proident dolor nisi ipsum. Sint velit ut Lorem dolore in ea eu anim et.",
+      "address": "70 Fleet Walk, Healy, New Mexico",
+      "company": "Fitcore",
+      "location": {
+        "lat": -88.452061,
+        "long": -143.523709
+      }
+    },
+    "username": "cindy92",
+    "createdAt": "2011-10-16T06:53:51.655Z",
+    "updatedAt": "2011-10-17T06:53:51.655Z"
+  }
+]
